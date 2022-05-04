de8f756814ebcb0cc863dfd195a0780d89391ff1
wkkobus <willkobus@gmail.com>
2022-05-04 16:32:11 -0400

    added comments section and get endpoint

---
+++ Routes/getMovie.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const axios = require("axios");
+const Movie = require("../Models/movie");
+const getMovieRouter = express.Router();
+getMovieRouter.use(express.json());
+
+getMovieRouter.get("/", async (req, res) => {
+    const {title} = req.query
+
+    if(title){
+        const movie = await Movie.findOne({title: title})
+        console.log(movie);
+        res.render("review", {movie})
+    }
+})
+
+module.exports = {
+    getMovieRouter
+}
