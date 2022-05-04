9b4456072ef1ef0282e5a233755fa3f0294cb8cb
samekh_resh <C0d1ng4Lyf3>
2022-05-04 16:35:27 -0400

    working with nelly nd moving things around.

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
-const goos = require("mongoose")
-const ejs = require("ejs")
-const {movieRouter} = require("./Routes/movieAPI")
+require("dotenv").config();
+const goos = require("mongoose");
+const ejs = require("ejs");
+const { movieRouter } = require("./Routes/movieAPI");
 
-const connect = async () => { await goos.connect(process.env.DB_URL, { useNewUrlParser: true })}
+const connect = async () => {
+  await goos.connect(process.env.DB_URL, { useNewUrlParser: true });
+};
 
 const server = express();
 
-server.use(express.static("public"))
-server.use(express.urlencoded({extended: true}))
-server.use(express.json())
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
-server.set("views", "./Views")
-server.set("view engine", "ejs")
+server.set("views", "./Views");
+server.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 3000;
 
 connect()
-.then( () => {
+  .then(() => {
     server.listen(PORT, () => {
-    console.log("Server Listening...");
-  });
-  server.use("/api", movieRouter)
+      console.log("Server Listening...");
+    });
+    server.use("/api", movieRouter);
 
-  server.get("/", (req, res) => {
+    server.get("/", (req, res) => {
       res.render("index");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-})
-.catch( (error) => {console.log(error)})
-
-
-
 
 // server.get("/review", (req, res) => {
 //     res.render("review")
